b2b369ea9575a6f9e19f59f1afe8ef632360b01f
Jonathan Sands <jonathan.sands@amaris.com>
2024-02-20 14:05:31 +0100

    Remove deprecated config files

--- rag/blocks/index/create_index.py
+++
@@ -1,9 +0,0 @@
-from config import *
-from indexing.rag_helper import LoadAndSplit,IndexConstructor
-
-if "docs" in config_index.keys():
-    search = IndexConstructor(**config_index)
-else:
-    docs = LoadAndSplit(**config_load_and_split)
-    search=IndexConstructor(docs=docs,**config_index)
-
